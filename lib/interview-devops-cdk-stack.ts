d61da07cc6f62603fab92de679e24e8047dd2832
Abner Ballardo <abnerballardo@ihfintech.com.pe>
2025-05-14 18:29:36 -0500

    feat: small improvements

--- lib/interview-devops-cdk-stack.ts
+++ lib/interview-devops-cdk-stack.ts
@@ -7,14 +7,14 @@ export class InterviewDevopsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambda = this.createLambda();
+    const lambda = this.createLambda('Customers');
     const api = this.createApiGateway();
     this.configureGetCustomers({ api, lambda });
   }
 
-  private createLambda() {
-    return new lambda.Function(this, 'CustomersLambda', {
-      functionName: `CustomersLambda`,
+  private createLambda(microservice: string) {
+    return new lambda.Function(this, `${microservice}Lambda`, {
+      functionName: `${microservice}Lambda`,
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: `dist/handlers/customers.customersHandler`,
       code: lambda.Code.fromInline(
@@ -40,8 +40,8 @@ export class InterviewDevopsCdkStack extends cdk.Stack {
     api: apiGateway.RestApi;
     lambda: lambda.Function;
   }) {
-    api.root
-      .resourceForPath('customers')
+    api.root // /
+      .resourceForPath('customers') // /customers
       .addMethod('GET', new apiGateway.LambdaIntegration(lambda));
   }
 }
